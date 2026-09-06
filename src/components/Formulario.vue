f00a89025d5254dad9487463c991ee9014ce3795
JonathanECH <jonathanecampos3@gmail.com>
2026-09-06 17:43:35 -0400

    Inicio de la app de clima.

---
+++ src/components/Formulario.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <form class="formulario">
+        <div class="campo">
+            <label for="ciudad">Ingresa la ciudad</label>
+            <input type="text" id="ciudad" placeholder="Ciudad">
+        </div>
+        <div class="campo">
+            <label for="pais">Selecciona el pais</label>
+            <select name="" id="pais">
+                <option value="" disabled selected>-- Seleccionar --</option>
+            </select>
+        </div>
+    </form>
+</template>
